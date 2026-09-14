46abf0a8d5e804e8285408a56e4700ed02760305
Nenerox <andres.ps0308@gmail.com>
2026-09-14 09:45:15 -0600

    Se agregaron los recursos adicionales

---
+++ App/app/src/main/java/plat/lab/applaboratorio/Character.kt
@@ -0,0 +1,10 @@
+package plat.lab.applaboratorio
+
+data class Character(
+    val id: Int,
+    val name: String,
+    val status: String,
+    val species: String,
+    val gender: String,
+    val image: String
+)
