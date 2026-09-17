b2ef0ecbce21868e095fa63a7c47facec4372847
Nenerox <andres.ps0308@gmail.com>
2026-09-16 18:27:56 -0600

    Se agregaron la imagen y muestra de informacion segun el id de personaje enviado

--- App/app/src/main/java/plat/lab/applaboratorio/CharacterDetails.kt
+++ App/app/src/main/java/plat/lab/applaboratorio/CharacterDetails.kt
@@ -1,7 +1,15 @@
 package plat.lab.applaboratorio
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -10,9 +18,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,7 +33,11 @@ fun CharacterDetails(modifier: Modifier = Modifier,
                      id: Int,
                      onBackArrow: () -> Unit){
     Column(modifier = modifier
-        .fillMaxSize()) {
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        val character: Character
+        character = CharacterDb().getCharacterById(id)
+
         TopAppBar(
             title = { Text("Character Detail") },
             navigationIcon = {
@@ -36,12 +53,49 @@ fun CharacterDetails(modifier: Modifier = Modifier,
                 titleContentColor = MaterialTheme.colorScheme.primary,
             )
         )
-        Text("Character Details id: $id")
+
+        AsyncImage(
+            model = character.image,
+            contentDescription = "Character Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(10.dp)
+                .size(220.dp)
+                .clip(CircleShape)
+        )
+
+        Text(character.name,
+            style = MaterialTheme.typography.titleLarge)
+
+        Row(modifier = modifier.fillMaxWidth()
+            .padding(top = 20.dp, start = 50.dp, end = 50.dp),
+            horizontalArrangement = Arrangement.SpaceBetween){
+            Text("Species: ",
+                style = MaterialTheme.typography.bodyLarge)
+            Text("${character.species}",
+                style = MaterialTheme.typography.bodyLarge)
+        }
+        Row(modifier = modifier.fillMaxWidth()
+            .padding(top = 10.dp, start = 50.dp, end = 50.dp),
+            horizontalArrangement = Arrangement.SpaceBetween){
+            Text("Status: ",
+                style = MaterialTheme.typography.bodyLarge)
+            Text("${character.status}",
+                style = MaterialTheme.typography.bodyLarge)
+        }
+        Row(modifier = modifier.fillMaxWidth()
+            .padding(top = 10.dp, start = 50.dp, end = 50.dp),
+            horizontalArrangement = Arrangement.SpaceBetween){
+            Text("Gender: ",
+                style = MaterialTheme.typography.bodyLarge)
+            Text("${character.gender}",
+                style = MaterialTheme.typography.bodyLarge)
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun CharacterDetailsPreview(){
-    CharacterDetails(id = 1, onBackArrow = {})
+    CharacterDetails(id = 2, onBackArrow = {})
 }
